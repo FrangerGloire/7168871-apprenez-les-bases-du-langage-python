721b175fd7c9e32c0335ca84a37d9768298bd2b1
FrangerGloire <132589833+FrangerGloire@users.noreply.github.com>
2024-05-12 05:04:21 -0400

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+# Création des quatres variables
+nom = "Gloire"
+age = 26
+taille = 1.66
+est_etudiant = True
+
+# Affichage des valeurs des quatres variables
+print(f"Nom: {nom}")
+print(f"Age: {age}")
+print(f"Taille: {taille}")
+print(f"Est étudiant: {est_etudiant}")
+
+# Affichage des types des quatres variables
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
