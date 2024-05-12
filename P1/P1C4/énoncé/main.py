9ea5b9bbeaba9751cb449cfe2ce15126d5e2e7ad
FrangerGloire <132589833+FrangerGloire@users.noreply.github.com>
2024-05-12 04:28:16 -0400

    Update main.py

--- P1/P1C4/énoncé/main.py
+++ P1/P1C4/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+# questions 1 à 3
+nom = "Gloire"
+age = 25
+print(f"je me prenomme {nom} et j'ai {age}ans")
+# questions 4 à 5
+age = 35
+print(f"je me prenomme {nom} et j'ai {age}ans actuellement")
